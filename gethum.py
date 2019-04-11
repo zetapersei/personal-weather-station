b62060e0e216c2abdc37b35b736acc68ed6ae7c1
Maurizio <zeta_persei@hotmail.com>
2019-04-11 13:53:31 +0200

    Update gethum.py

--- gethum.py
+++ gethum.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import re, os, rrdtool, time, Adafruit_DHT
+import re, os, rrdtool, time, Adafruit_DHT, MySQLdb
 
-humidity, temperature = Adafruit_DHT.read_retry(11, 14)
-
-# read sensor data
-data = 'N'
+humidity, temperature = Adafruit_DHT.read_retry(11, 14) print (humidity) # read sensor data data = 'N'
 
 data += ':'
-data += humidity
+data += str(humidity)
 time.sleep(1)
 
 # insert data into round-robin-database
 rrdtool.update(
   "/home/pi/personal-weather-station/humidity.rrd",
 data)
+# Connessione al Database
+db = MySQLdb.connect("localhost","pico","maurizio","weather" ) # Ottenimento del cursore cursor = db.cursor() sql = "INSERT INTO wr_humidity (sensor_id, value) " "VALUES(6, %u)" % humidity
+
+try:
+   # Esecuzione della query SQL
+   cursor.execute(sql)
+   # Commit
+   db.commit()
+except:
+# Rollback in caso di errore
+   db.rollback()
+
+# Disconnessione
+db.close()
