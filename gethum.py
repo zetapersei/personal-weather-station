b261892b5d99e6438b33a8f36d19fb96515e361f
Maurizio <zeta_persei@hotmail.com>
2019-04-11 20:54:26 +0200

    Update gethum.py

--- gethum.py
+++ gethum.py
@@ -13,8 +13,12 @@ time.sleep(1)
 rrdtool.update(
   "/home/pi/personal-weather-station/humidity.rrd",
 data)
-# Coonection Database Mysq
-db = MySQLdb.connect("localhost","user","passwd","weather" ) # Ottenimento del cursore cursor = db.cursor() sql = "INSERT INTO wr_humidity (sensor_id, value) " "VALUES(6, %u)" % humidity
+# Connection Database Mysql
+db = MySQLdb.connect("localhost","user","passwd","weather") 
+# Ottenimento del cursore 
+cursor = db.cursor() 
+
+sql = "INSERT INTO wr_humidity (sensor_id, value) " "VALUES(6, %u)" % humidity
 
 try:
    # Esecuzione della query SQL
